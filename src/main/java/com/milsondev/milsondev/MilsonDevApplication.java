9bc7696261f96046b11b9ecd1afc90344df99a77
milson.antonio <milson.milson@outlook.com>
2022-08-04 23:39:43 +0200

    Initial commit

---
+++ src/main/java/com/milsondev/milsondev/MilsonDevApplication.java
@@ -0,0 +1,11 @@
+package com.milsondev.milsondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilsonDevApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MilsonDevApplication.class, args);
+	}
+}
---
+++ src/test/java/com/milsondev/milsondev/MilsonDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milsondev.milsondev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilsonDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
