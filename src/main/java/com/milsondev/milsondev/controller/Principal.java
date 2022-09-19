a196754aafa49e81598e81316852473b1ad294c0
milson.antonio <milson.antonio@favendo.com>
2022-09-19 19:48:02 +0200

    change articles directory

--- src/main/java/com/milsondev/milsondev/controller/Principal.java
+++ src/main/java/com/milsondev/milsondev/controller/Principal.java
@@ -130,7 +130,7 @@ public class Principal {
     public ModelAndView openArticle(@PathVariable String page, RedirectAttributes attributes) {
         Article article = articleService.getArticleByHtmlPage(page).get();
 
-        ModelAndView mv = new ModelAndView(article.getPath()+page);
+        ModelAndView mv = new ModelAndView(page);
 
         mv.addObject("article", article);
         mv.addObject("title", article.getTitle());
